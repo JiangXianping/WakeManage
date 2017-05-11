52bc0dd62621be32d561d5487cea313759bd4a8c
JiangXianping <992386253@qq.com>
2017-05-11 22:47:06 +0800

    使用pageHelper插件完成分页效果，在mybatis-config中配置相对应的信息

--- ssm-web/src/main/java/com/jiang/controller/BookController.java
+++ ssm-web/src/main/java/com/jiang/controller/BookController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.google.gson.Gson;
 import com.jiang.mapper.BookMapper;
 import com.jiang.model.Book;
@@ -23,20 +26,28 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	/**
-	 * 
+	 * 分页查询所有图书，使用pageHelper插件完成
 	 * @return
 	 */
 	@RequestMapping("/bookList")
-	public ModelAndView bookList(){
+	public ModelAndView bookList(ModelAndView mv,
+			@RequestParam(required=true,defaultValue="1") Integer page,
+			@RequestParam(required=true,defaultValue="5") Integer pageSize){
 		
 		Gson gson = new Gson();
-		
-		ModelAndView mv = new ModelAndView();
+		//表示要开始分页了
+		//page：从page页开始
+		//pageSize：每页显示pageSize条记录
+		PageHelper.startPage(page, pageSize);
+		mv = new ModelAndView();
 		List<Book> list = bookService.findAllBook();
+		PageInfo<Book> pageInfo = new PageInfo<Book>(list);
+		
+		mv.addObject("pageInfo", pageInfo);
+		mv.addObject("bookList", list);
 		
 		System.out.println(gson.toJson(list));
-		mv.addObject("bookList", list);
-		mv.setViewName("bookList.jsp");
+		mv.setViewName("/WEB-INF/pages/bookList.jsp");
 
 		return mv;
 	}
